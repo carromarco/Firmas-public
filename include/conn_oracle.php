815bc92bd100c71979bbace06cdee5faa7ee0a34
Marco Carro <117318209+carromarco@users.noreply.github.com>
2024-06-24 19:44:53 -0300

    Update conn_oracle.php

--- include/conn_oracle.php
+++ include/conn_oracle.php
@@ -1,19 +1,1 @@
-<?php 
 
-# Usuario que recibo desde el form que esta en index.php
-
-$usuario = strtoupper ($_POST["usuario"]);
-
-# Password que recibo desde el form que esta en index.php
-
-
-$clave = md5($_POST["clave"]);
-
-
-# Nombre de la base de datos a la cual me conecto
-
-
-$base = "PMPROD10";
-
-
-?>
