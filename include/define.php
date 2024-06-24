ef1bbb982d7a890c37ddda347f3034d83caf3a45
Marco Carro <117318209+carromarco@users.noreply.github.com>
2024-06-24 19:43:59 -0300

    Update define.php
    Confidencial

--- include/define.php
+++ include/define.php
@@ -1,20 +1,2 @@
 <?php 
 
-# Usuario que recibo desde el form que esta en index.php
-
-$usuario = 'nu0';
-
-# Password que recibo desde el form que esta en index.php
-
-$clave = 'c0nu';
-
-# Nombre de la base de datos a la cual me conecto
-
-$db10 = "PM10PROD";
-
-# Error de conexion a oracle
-
-$msg_error01 = "ORA-01017: Nombre de usuario/contraseña no válidos. Conexión denegada."
-
-
-?>
